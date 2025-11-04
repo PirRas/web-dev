ff7fd5b017944c5af4cf12e2bb9105ec90008663
PirRas <pkaschuso@gmail.com>
2025-11-04 15:52:49 +0100

    Bis Aufgabe 12

--- htmlFiles/Übung JavaScript/script.js
+++ htmlFiles/Übung JavaScript/script.js
@@ -1,14 +1,9 @@
-console.log("Kallo Kanti Olten, ich bin Piravin");
-let number = 17;
-let multiplier = 28;
-let result = number * multiplier;
-console.log("Die Zahl", number, "mit der Zahl", multiplier, "multipliziert gibt", number * multiplier);
-
-console.log(result);
-console.log("result");
-
-let myName = "Piravin";
-let myMotto = "YOLO";
-let myFavSubjects = ["Informatik", "Mathematik", "SF Biologie", "SF Chemie"];
-let myAge = 17;
-console.log("Ich heisse", myName + "Mein Motto ist:", myMotto, "Meine Lieblingsfächer sind", myFavSubjects, "Mein Alter ist", myAge);
+let mottoButton = document.getElementById("displayMottoButton");
+function displayMotto() {
+    let newElement = document.createElement("p");
+    newElement.id="meinMotto";
+    newElement.innerHTML="YOLO";
+    let existingParentElement = document.getElementById("aboutMe");
+    existingParentElement.appendChild(newElement);
+}
+mottoButton.addEventListener("click", displayMotto);
