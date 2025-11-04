435b98ec98c1eb4d0e6c9df566c67eb748508bee
PirRas <pkaschuso@gmail.com>
2025-11-04 15:38:42 +0100

    Bis Aufgabe 8

---
+++ htmlFiles/Übung JavaScript/script.js
@@ -0,0 +1,14 @@
+console.log("Kallo Kanti Olten, ich bin Piravin");
+let number = 17;
+let multiplier = 28;
+let result = number * multiplier;
+console.log("Die Zahl", number, "mit der Zahl", multiplier, "multipliziert gibt", number * multiplier);
+
+console.log(result);
+console.log("result");
+
+let myName = "Piravin";
+let myMotto = "YOLO";
+let myFavSubjects = ["Informatik", "Mathematik", "SF Biologie", "SF Chemie"];
+let myAge = 17;
+console.log("Ich heisse", myName + "Mein Motto ist:", myMotto, "Meine Lieblingsfächer sind", myFavSubjects, "Mein Alter ist", myAge);
