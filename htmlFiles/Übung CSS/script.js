6f76aefb7f013999ccfa463542a47b54a2ce2916
PirRas <pkaschuso@gmail.com>
2025-11-11 15:58:10 +0100

    CSS - Introduction bis Aufgabe 4

---
+++ htmlFiles/Übung CSS/script.js
@@ -0,0 +1,66 @@
+// Globale Variablen
+const pokemonBilderQuelle = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/";
+
+const pokemons = [
+  { name: "Pikachu", type: "Elektro", image: `${pokemonBilderQuelle}25.png` },
+  { name: "Glumanda", type: "Feuer", image: `${pokemonBilderQuelle}4.png` },
+  { name: "Schiggy", type: "Wasser", image: `${pokemonBilderQuelle}7.png` },
+  { name: "Bisasam", type: "Pflanze", image: `${pokemonBilderQuelle}1.png` },
+  { name: "Evoli", type: "Normal", image: `${pokemonBilderQuelle}133.png` }
+];
+
+
+// HTML-Elemente
+let firstSection = document.createElement("div");
+firstSection.id = "title-section";
+firstSection.classList.add("container");
+document.body.appendChild(firstSection);
+
+let title = document.createElement("h1");
+title.id = "title";
+title.innerText = "Pokédex";
+firstSection.appendChild(title);
+
+let pokemonCards = document.createElement("div");
+pokemonCards.id = "pokemon-cards";
+pokemonCards.classList.add("container");
+document.body.appendChild(pokemonCards);
+
+
+for (const pokemon of pokemons){
+
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const title = document.createElement("h2");
+    title.textContent = pokemon.name;
+
+    const image = document.createElement("img");
+    image.src = pokemon.image;
+
+    const type = document.createElement("p");
+    type.textContent = "Typ: " + pokemon.type;
+
+    card.appendChild(title);
+    card.appendChild(image);
+    card.appendChild(type);
+
+    pokemonCards.appendChild(card);
+}
+
+// Funktionen
+
+
+const cities = ["Paris", "London", "Berlin"];
+
+cities.forEach(city => {
+  console.log(city);
+  console.log(city.length);
+});
+
+
+
+
+
+
+// Event-Listeners
